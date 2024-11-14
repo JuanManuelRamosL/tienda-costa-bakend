d3d0c591b3f6ea230caa9f7afa9307e60dc7882d
juanmanuel <juanmanuelramoslencina@gmail.com>
2024-11-13 23:21:50 -0300

    error de cors?

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(cors("*"));
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(morgan("dev"))
